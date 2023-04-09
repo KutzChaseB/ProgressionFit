d4ea7eb28fa601473762929598172e7f3dc5b931
peterstrongg <peterstrongdev@outlook.com>
2023-04-09 18:06:20 -0400

    Back to dashboard button added to healthinfo

--- frontend/src/components/healthinfo/healthinfo.js
+++ frontend/src/components/healthinfo/healthinfo.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import DashButton from "../../assets/dashbutton";
+
 const HealthInfo = () => {
     return (
-        <div className="flex flex-row min-h-screen justify-center items-center bg-pf-gray text-pf-white">
+        <div className="flex flex-col min-h-screen justify-center items-center bg-pf-gray text-pf-white">
             <h1>Health Info</h1>
+            {/* <DashButton text="Back to Dashboard" redirect="/dashboard" /> */}
         </div>
     );
 }
