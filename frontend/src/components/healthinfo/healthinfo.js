4aa0177160e2e4b0ddc512b649c60b3055606cb7
JoelTul <128608362+JoelTul@users.noreply.github.com>
2023-04-10 09:43:28 -0400

    uncommented button

--- frontend/src/components/healthinfo/healthinfo.js
+++ frontend/src/components/healthinfo/healthinfo.js
@@ -6,7 +6,7 @@ const HealthInfo = () => {
     return (
         <div className="flex flex-col min-h-screen justify-center items-center bg-pf-gray text-pf-white">
             <h1>Health Info</h1>
-            {/* <DashButton text="Back to Dashboard" redirect="/dashboard" /> */}
+             <DashButton text="Back to Dashboard" redirect="/dashboard" />
         </div>
     );
 }
