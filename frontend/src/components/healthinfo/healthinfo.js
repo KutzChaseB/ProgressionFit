dcc72512628378cbca2a6ed7f5f20135c40014ae
Peter Strong <peterstrongdev@outlook.com>
2023-04-10 14:08:29 -0400

    Boiler plate code for pages

--- frontend/src/components/healthinfo/healthinfo.js
+++ frontend/src/components/healthinfo/healthinfo.js
@@ -5,8 +5,13 @@ import DashButton from "../../assets/dashbutton";
 const HealthInfo = () => {
     return (
         <div className="flex flex-col min-h-screen justify-center items-center bg-pf-gray text-pf-white">
-            <h1>Health Info</h1>
-             <DashButton text="Back to Dashboard" redirect="/dashboard" />
+            <div className="flex flex-col justify-center items-center bg-pf-field text-pf-gray px-5 py-5 rounded-md">
+                <h1 className="text-xl font-bold mb-2">Health Info</h1>
+                <div>
+                    {/* Fields go here */}
+                </div>
+                <DashButton text="Back to Dashboard" redirect="/dashboard" />
+            </div>
         </div>
     );
 }
--- frontend/src/components/trackprogress/progress.js
+++ frontend/src/components/trackprogress/progress.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+import DashButton from "../../assets/dashbutton";
+
 const Progress = () => {
     return (
-        <div className="flex flex-row min-h-screen justify-center items-center bg-pf-gray text-pf-white">
-            <h1>Track Progress</h1>
+        <div className="flex flex-col min-h-screen justify-center items-center bg-pf-gray text-pf-white">
+            <div className="flex flex-col justify-center items-center bg-pf-field text-pf-gray px-5 py-5 rounded-md">
+                <h1 className="text-xl font-bold mb-2">Track Progress</h1>
+                <div>
+                    {/* Fields go here */}
+                </div>
+                <DashButton text="Back to Dashboard" redirect="/dashboard" />
+            </div>
         </div>
     );
 }
