6e338ae9fd8f52a3560a970e526d9b330e56642b
Peter Strong <peterstrongdev@outlook.com>
2023-09-15 09:15:28 -0400

    added Sign Out button

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -5,6 +5,14 @@ import DashButton from "../../assets/dashbutton";
 
 const Dashboard = () => {
     const { sessionInfo, setSessionInfo } = useContext(SessionInfo);
+
+    const signOut = () => {
+        setSessionInfo({
+            "id" : 0,
+            "username" : "",
+        })
+    };
+
     return (
         <div className="flex flex-row min-h-screen justify-center items-center bg-pf-gray text-pf-white">
             <div className="flex flex-col justify-center items-center bg-pf-field text-pf-white px-5 py-5 rounded-md">
@@ -14,6 +22,7 @@ const Dashboard = () => {
                 <DashButton text="Workout" redirect="/workout" />
                 <DashButton text="Track Progress" redirect="/progress" />
                 <DashButton text="Social" redirect="/social" />
+                <DashButton text="Sign Out" action={signOut} />
             </div>
         </div>
     );
