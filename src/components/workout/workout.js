d0e3e9362e845f2ec2bc6ff4805e2d4b988955eb
Peter Strong <peterstrongdev@outlook.com>
2023-09-26 08:35:42 -0400

    workout api call

--- src/components/workout/workout.js
+++ src/components/workout/workout.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import DashButton from "../../assets/dashbutton";
 
 const Workout = () => {
+    const [workoutList, setWorkoutList] = useState([]);
+
+    useEffect(() => {
+        fetch("/api/workouts", {
+            method : "GET",
+        }).then(
+            res => res.json()
+        ).then(
+            data => {
+                console.log(data);
+                setWorkoutList(data)
+            }
+        )
+    }, [])
+
     return (
         <div className="flex flex-col min-h-screen justify-center items-center bg-pf-gray text-pf-white">
             <div className="flex flex-col justify-center items-center bg-pf-field text-pf-gray px-5 py-5 rounded-md">
