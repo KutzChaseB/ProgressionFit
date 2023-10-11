ba6e9d33ef785d82db5b4b22b80967e4bd0e230e
Chase Blecker <cblec423@live.kutztown.edu>
2023-10-11 10:47:18 -0400

    minor bug fix

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -53,7 +53,7 @@ const Signup = () => {
                     <label className="block text-gray-700 text-sm font-bold mb-2">
                         Password
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Username" value={password} onChange={handlePassword}/>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password" value={password} onChange={handlePassword}/>
                     </div>
                     <div className="mb-6">
                     <label className="block text-gray-700 text-sm font-bold mb-2">
