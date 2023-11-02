db213dc90e67acb9e81155051006a59571998691
JoelTul <128608362+JoelTul@users.noreply.github.com>
2023-11-02 19:20:59 -0400

    popup share box

--- src/components/workout/workout.js
+++ src/components/workout/workout.js
@@ -1,18 +1,24 @@
 import React, { useState, useEffect, useContext } from "react";
 import { SessionInfo } from "../context/context";
 import DashButton from "../../assets/dashbutton";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 
 
 const Workout = () => {
-    const { sessionInfo, setSessionInfo } = useContext(SessionInfo);
+  const { sessionInfo } = useContext(SessionInfo);
 
-    const [workoutHistory, setWorkoutHistory] = useState([]);
-    const [workoutList, setWorkoutList] = useState([]);
-    const [lift, setLift] = useState("");
-    const [repetitions, setRepetitions] = useState("");
-    const [sets, setSets] = useState("");
-    const [selectedWorkout, setSelectedWorkout] = useState("");
+  const [workoutHistory, setWorkoutHistory] = useState([]);
+  const [workoutList, setWorkoutList] = useState([]);
+  const [lift, setLift] = useState("");
+  const [repetitions, setRepetitions] = useState("");
+  const [sets, setSets] = useState("");
+  const [selectedWorkout, setSelectedWorkout] = useState("");
+  const [showCommentDialog, setShowCommentDialog] = useState(false);
+  const [commentText, setCommentText] = useState("");
+  const [workoutToShare, setWorkoutToShare] = useState(null);
+  const [isCommentModalOpen, setCommentModalOpen] = useState(false);
 
     const getWorkoutHistory = () => {
       fetch("/api/workout_history", {
@@ -100,104 +106,140 @@ const Workout = () => {
       }
     };
 
-    const shareWorkout = (wid, comment) => {
-      fetch("/api/share_workout", {
-        method : "POST",
-        cache : "no-cache",
-        headers : {
-          "content-type" : "application/json"
-        },
-        body : JSON.stringify({
-          "workout_id" : wid,
-          "user_id" : sessionInfo["id"],
-          "comment" : comment,
-        })
-      })
+    const shareWorkout = (wid) => {
+      setCommentModalOpen(true);
+      setWorkoutToShare(wid);
     };
-    
-    return (
-        <div className="flex flex-col min-h-screen justify-center items-center bg-pf-gray text-pf-white">
-            <div className="flex flex-col justify-center items-center bg-pf-field text-pf-gray px-5 py-5 rounded-md">
-                <h1 className="text-xl font-bold mb-2">Workout</h1>
-              <div>
-                <div>   
-                    <select
-                      value = {selectedWorkout}
-                      onChange = {handleSelectedWorkoutChange}
-                    >
-                      {workoutList.map((w) => <option value={w[0]}>{w[0]}</option>)}
-                    </select>                     
-            
-                    <label className="block text-gray-700 text-sm font-bold mb-2">
-                      Sets
-                    </label>
-                    <input
-                      type="text"
-                      value={sets}
-                      onChange={handleSetsChange}
-                      className="border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                      placeholder="Enter Sets"
-                    />
-                    <label className="block text-gray-700 text-sm font-bold mb-2">
-                      Repetitions
-                    </label>
-                    <input
-                      type="text"
-                      value={repetitions}
-                      onChange={handleRepetitionsChange}
-                      className="border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                      placeholder="Enter Repetitions"
-                    />
-                    <label className="block text-gray-700 text-sm font-bold mb-2">
-                      Lift
-                    </label>
-                    <input
-                      type="text"
-                      value={lift}
-                      onChange={handleLiftChange}
-                      className="border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                      placeholder="Enter Lift"
-                    />
-
-
-                </div>
-              </div>
-                
-              <DashButton text="+" action = {logWorkout} />
-        
-              <div>
-                <h1>Workout Data</h1>
-                <table>
-                  <thead>
-                    <tr>
-                      <th>Date</th>
-                      <th>Exercise Name</th>
-                      <th>Reps</th>
-                      <th>Sets</th>
-                      <th>Weight</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {workoutHistory.map((workout, index) => (
-                      <tr key={index}>
-                        <td>{workout.date}</td>
-                        <td>{workout.exerciseName}</td>
-                        <td>{workout.reps}</td>
-                        <td>{workout.sets}</td>
-                        <td>{workout.weight}</td>
-                        <DashButton text="Share" action={() => {shareWorkout(workout.logId, "Comment")}}/>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-
-              <DashButton text="Back to Dashboard" redirect="/dashboard" />
-            </div>
-        </div>
-    );
-   
+    const handleCommentChange = (event) => {
+      const inputComment = event.target.value;
       
-}
+      if (inputComment.length <= 100) {
+        setCommentText(inputComment);
+      }
+    };
+    const handleSubmitComment = () => {
+      //Peter I think you do api call here
+      setCommentModalOpen(false);
+    };
+  
+    const shareWorkoutWithComment = () => {
+     
+      setShowCommentDialog(false);
+      setCommentText("");
+    };
+    return (
+      <div className="flex flex-col min-h-screen justify-center items-center bg-pf-gray text-pf-white">
+          <div className="flex flex-col justify-center items-center bg-pf-field text-pf-gray px-5 py-5 rounded-md">
+              <h1 className="text-xl font-bold mb-2">Workout</h1>
+            <div>
+              <div>   
+                  <select
+                    value = {selectedWorkout}
+                    onChange = {handleSelectedWorkoutChange}
+                  >
+                    {workoutList.map((w) => <option value={w[0]}>{w[0]}</option>)}
+                  </select>                     
+          
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Sets
+                  </label>
+                  <input
+                    type="text"
+                    value={sets}
+                    onChange={handleSetsChange}
+                    className="border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    placeholder="Enter Sets"
+                  />
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Repetitions
+                  </label>
+                  <input
+                    type="text"
+                    value={repetitions}
+                    onChange={handleRepetitionsChange}
+                    className="border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    placeholder="Enter Repetitions"
+                  />
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Lift
+                  </label>
+                  <input
+                    type="text"
+                    value={lift}
+                    onChange={handleLiftChange}
+                    className="border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    placeholder="Enter Lift"
+                  />
 
-export default Workout;
+
+              </div>
+            </div>
+  
+          <DashButton text="+" action={logWorkout} />
+  
+          <div>
+            <h1>Workout Data</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Exercise Name</th>
+                  <th>Reps</th>
+                  <th>Sets</th>
+                  <th>Weight</th>
+                </tr>
+              </thead>
+              <tbody>
+                {workoutHistory.map((workout, index) => (
+                  <tr key={index}>
+                    <td>{workout.date}</td>
+                    <td>{workout.exerciseName}</td>
+                    <td>{workout.reps}</td>
+                    <td>{workout.sets}</td>
+                    <td>{workout.weight}</td>
+                    <DashButton
+                      text="Share"
+                      action={() => shareWorkout(workout.logId)}
+                    />
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+  
+          <DashButton text="Back to Dashboard" redirect="/dashboard" />
+  
+          {showCommentDialog && (
+            <div className="comment-dialog">
+              <textarea
+                value={commentText}
+                onChange={handleCommentChange}
+                placeholder="Enter a comment (up to 100 characters)"
+              />
+              <button onClick={shareWorkoutWithComment}>Share</button>
+              <button onClick={() => setShowCommentDialog(false)}>Cancel</button>
+            </div>
+          )}
+        <Popup open={isCommentModalOpen} modal closeOnDocumentClick={false}>
+          {(close) => (
+            <div className="comment-modal">
+              <textarea
+                value={commentText}
+                onChange={handleCommentChange}
+                placeholder="Enter a comment (up to 100 characters)"
+              />
+              <div className="comment-modal-buttons">
+                <button onClick={handleSubmitComment}>Submit</button>
+                <button onClick={() => setCommentModalOpen(false)}>Cancel</button>
+              </div>
+            </div>
+          )}
+        </Popup>
+      </div>
+    </div>
+      
+      
+    );
+  };
+  
+  export default Workout;
