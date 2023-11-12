42f965d591e359af757567978d099d5830b57379
JoelTul <128608362+JoelTul@users.noreply.github.com>
2023-11-12 14:38:03 -0500

    initial submit tip page

--- src/components/submittip/submittip.js
+++ src/components/submittip/submittip.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+/*import React, { useState, useContext, useEffect } from 'react';
 import { SessionInfo } from "../context/context";
 import DashButton from '../../assets/dashbutton';
 
@@ -19,18 +19,111 @@ const SubmitTip = () => {
             })
         });
     };
+    
 
+    const handleTipChange = (event) => {
+      setTip(event.target.value);
+    };
+  
     return (
-        <div className="flex flex-col min-h-screen justify-center items-center bg-pf-gray text-pf-white">
+      <div className="flex flex-col min-h-screen justify-center items-center bg-pf-gray text-pf-white">
         <div className="flex flex-col justify-center items-center bg-pf-field text-pf-gray px-5 py-5 rounded-md">
-            <h1 className="text-xl font-bold mb-2">Submit a Tip</h1>
+          <h1 className="text-xl font-bold mb-2">Submit a Tip</h1>
+          
+          <form onSubmit={submitHandler}>
             <div>
-                {/* Fields go here */}
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Enter Tip
+                    </label>
+              <textarea
+                id="tip"
+                name="tip"
+                value={tip}
+                onChange={handleTipChange}
+                className="w-full"
+              />
             </div>
-            <DashButton text="Back" redirect="/social" />
+            <DashButton text="Submit" type="submit" />
+          </form>
+          <DashButton text="Back" redirect="/social" />
         </div>
+      </div>
+    );
+  };
+
+
+export default SubmitTip;
+*/
+import React, { useState, useContext } from 'react';
+import { SessionInfo } from '../context/context';
+import DashButton from '../../assets/dashbutton';
+
+const SubmitTip = () => {
+  const { sessionInfo, setSessionInfo } = useContext(SessionInfo);
+  const [tip, setTip] = useState('');
+
+  const submitHandler = async (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    try {
+      const response = await fetch('/api/tips', {
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          user_id: sessionInfo['id'],
+          tip: tip,
+        }),
+      });
+
+      if (response.ok) {
+        // Assuming the server responds with a success status
+        // You can handle the success, for example, redirect to the tips page
+       // window.location.href = '/social/tips'; // Replace with your actual tips page URL
+      } else {
+        // error check
+        console.error('Failed to submit tip');
+      }
+    } catch (error) {
+      console.error('Error submitting tip:', error);
+    }
+  };
+
+  const handleTipChange = (event) => {
+    setTip(event.target.value);
+  };
+
+  return (
+    <div className="flex flex-col min-h-screen justify-center items-center bg-pf-gray text-pf-white">
+      <div className="flex flex-col justify-center items-center bg-pf-field text-pf-gray px-5 py-5 rounded-md">
+        <h1 className="text-xl font-bold mb-2">Submit a Tip</h1>
+
+        <form onSubmit={submitHandler}>
+          <div>
+            <label className="block text-gray-700 text-sm font-bold mb-2">Enter Tip</label>
+            <textarea
+              id="tip"
+              name="tip"
+              value={tip}
+              onChange={handleTipChange}
+              className="w-full"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          >
+            Submit
+          </button>
+          <DashButton text="Back" redirect="/social" />
+        </form>
+
+        
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default SubmitTip;
--- src/components/tips/tips.js
+++ src/components/tips/tips.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+/*import React, { useState, useEffect } from "react";
 
 import DashButton from "../../assets/dashbutton";
 import TipNode from "../../assets/tipnode";
@@ -32,5 +32,55 @@ const Tips = () => {
         </div>
     );
 }
+
+export default Tips;*/
+
+import React, { useState, useEffect } from "react";
+import DashButton from "../../assets/dashbutton";
+import TipNode from "../../assets/tipnode";
+
+const Tips = () => {
+  const [tips, setTips] = useState([]);
+
+  useEffect(() => {
+    // Fetch tips initially
+    fetchTips();
+
+    // Listen for the custom event
+    document.addEventListener('tipSubmitted', fetchTips);
+
+    // Cleanup the event listener when the component is unmounted
+    return () => {
+      document.removeEventListener('tipSubmitted', fetchTips);
+    };
+  }, []);
+
+  const fetchTips = () => {
+    fetch("/api/tips", {
+      method: "GET",
+    })
+      .then(res => res.json())
+      .then(data => {
+        setTips(data);
+      })
+      .catch(error => {
+        console.error('Error fetching tips:', error);
+      });
+  };
+
+  return (
+    <div className="flex flex-col min-h-screen justify-center items-center bg-pf-gray text-pf-white">
+      <div className="flex flex-col justify-center items-center bg-pf-field text-pf-gray px-5 py-5 rounded-md">
+        <h1 className="text-xl font-bold mb-2">Tips</h1>
+        <div>
+          {tips.map((t, index) => (
+            <TipNode key={index} user={t[0]} tip={t[1]} />
+          ))}
+        </div>
+        <DashButton text="Back" redirect="/social" />
+      </div>
+    </div>
+  );
+};
 
 export default Tips;
